742f3a1bb871811ea22d4d5342c4839bc1a052f6
Patchouli <self@patchouli.sh>
2024-11-07 13:40:49 -0500

    Added DI extension project files

---
+++ source/Firecrawl.Extensions.DependencyInjection/FirecrawlServiceOptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Firecrawl
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public sealed class FirecrawlServiceOptions
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		public Uri BaseUrl { get; set; } =
+			FirecrawlService.DefaultBaseUrl;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public string ApiKey { get; set; }
+	}
+}
---
+++ source/Firecrawl.Extensions.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+using Microsoft;
+using Microsoft.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Firecrawl
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public static class ServiceCollectionExtensions
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		private const string FirecrawlHttpClientName =
+			"firecrawl";
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="services"></param>
+		/// <param name="optionsCallback"></param>
+		/// <returns></returns>
+		public static IHttpClientBuilder AddFirecrawlHttpClient(
+			this IServiceCollection services,
+			Action<FirecrawlServiceOptions> optionsCallback = default)
+		{
+			ArgumentNullException
+				.ThrowIfNull(
+					services,
+					nameof(services));
+
+			var options =
+				new FirecrawlServiceOptions();
+
+			return services
+				.AddHttpClient<IFirecrawlService, FirecrawlService>(
+					FirecrawlHttpClientName,
+					(httpClient) =>
+					{
+						// Configure the base URL for the client.
+						httpClient.BaseAddress =
+							options.BaseUrl;
+
+						// Configure the default request headers for the client.
+						httpClient.DefaultRequestHeaders.Authorization =
+							new AuthenticationHeaderValue(
+								"Bearer",
+								options.ApiKey);
+					});
+		}
+	}
+}
