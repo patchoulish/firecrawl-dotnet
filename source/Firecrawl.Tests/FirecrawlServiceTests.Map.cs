a46d15d3907842d1bf77ca26500190fd9af1b305
Patchouli <self@patchouli.sh>
2024-11-07 14:32:13 -0500

    Added unit tests

--- source/Firecrawl.Tests/FirecrawlServiceTests.Map.cs
+++ source/Firecrawl.Tests/FirecrawlServiceTests.Map.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
 
 using Microsoft;
 using Microsoft.VisualStudio;
@@ -11,6 +13,33 @@ namespace Firecrawl
 {
 	partial class FirecrawlServiceTests
 	{
+		/// <summary>
+		/// 
+		/// </summary>
+		public static IEnumerable<object[]> MapThrowOnBadRequestTestData =>
+			[
+				[
+					new FirecrawlMapOptions()
+					{
+						Url = new Uri("https://www.reddit.com/"),
+					}
+				],
+			];
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static IEnumerable<object[]> MapTestData =>
+			[
+				[
+					new FirecrawlMapOptions()
+					{
+						Url = new Uri("https://en.wikipedia.org/wiki/Main_Page"),
+						Query = "Featured Article"
+					}
+				],
+			];
+
 		/// <summary>
 		/// 
 		/// </summary>
@@ -27,6 +56,53 @@ namespace Firecrawl
 			await Assert.ThrowsExceptionAsync<ArgumentNullException>(
 				() => this.firecrawl.MapAsync(
 					default));
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="options"></param>
+		/// <returns></returns>
+		[TestCategory(
+			"Map")]
+		[TestCategory(
+			"BadRequest")]
+		[DataTestMethod]
+		[DynamicData(
+			nameof(MapThrowOnBadRequestTestData))]
+		public async Task MapThrowOnBadRequestAsync(
+			FirecrawlMapOptions options)
+		{
+			await Assert.ThrowsExceptionAsync<FirecrawlException>(
+				() => this.firecrawl.MapAsync(
+					options));
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="options"></param>
+		/// <returns></returns>
+		[TestCategory(
+			"Map")]
+		[TestCategory(
+			"Result")]
+		[DataTestMethod]
+		[DynamicData(
+			nameof(MapTestData))]
+		public async Task MapAsync(
+			FirecrawlMapOptions options)
+		{
+			var mapResult =
+				await this.firecrawl
+					.MapAsync(
+						options);
+
+			Assert.IsNotNull(
+				mapResult);
+
+			Assert.IsTrue(
+				mapResult.Success);
 		}
 	}
 }
--- source/Firecrawl.Tests/FirecrawlServiceTests.Scrape.cs
+++ source/Firecrawl.Tests/FirecrawlServiceTests.Scrape.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
 
 using Microsoft;
 using Microsoft.VisualStudio;
@@ -11,6 +13,40 @@ namespace Firecrawl
 {
 	partial class FirecrawlServiceTests
 	{
+		/// <summary>
+		/// 
+		/// </summary>
+		public static IEnumerable<object[]> ScrapeThrowOnBadRequestTestData =>
+			[
+				[
+					new FirecrawlScrapeOptions()
+					{
+						Url = new Uri("https://www.reddit.com/"),
+						Formats =
+						[
+							FirecrawlFormat.Markdown
+						],
+					}
+				],
+			];
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static IEnumerable<object[]> ScrapeTestData =>
+			[
+				[
+					new FirecrawlScrapeOptions()
+					{
+						Url = new Uri("https://en.wikipedia.org/wiki/Main_Page"),
+						Formats =
+						[
+							FirecrawlFormat.Markdown
+						],
+					}
+				],
+			];
+
 		/// <summary>
 		/// 
 		/// </summary>
@@ -27,6 +63,53 @@ namespace Firecrawl
 			await Assert.ThrowsExceptionAsync<ArgumentNullException>(
 				() => this.firecrawl.ScrapeAsync(
 					default));
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="options"></param>
+		/// <returns></returns>
+		[TestCategory(
+			"Scrape")]
+		[TestCategory(
+			"BadRequest")]
+		[DataTestMethod]
+		[DynamicData(
+			nameof(ScrapeThrowOnBadRequestTestData))]
+		public async Task ScrapeThrowOnBadRequestAsync(
+			FirecrawlScrapeOptions options)
+		{
+			await Assert.ThrowsExceptionAsync<FirecrawlException>(
+				() => this.firecrawl.ScrapeAsync(
+					options));
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="options"></param>
+		/// <returns></returns>
+		[TestCategory(
+			"Scrape")]
+		[TestCategory(
+			"Result")]
+		[DataTestMethod]
+		[DynamicData(
+			nameof(ScrapeTestData))]
+		public async Task ScrapeAsync(
+			FirecrawlScrapeOptions options)
+		{
+			var scrapeResult =
+				await this.firecrawl
+					.ScrapeAsync(
+						options);
+
+			Assert.IsNotNull(
+				scrapeResult);
+
+			Assert.IsTrue(
+				scrapeResult.Success);
 		}
 	}
 }
