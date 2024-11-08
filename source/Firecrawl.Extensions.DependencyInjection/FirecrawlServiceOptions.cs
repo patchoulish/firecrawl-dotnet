1df887e82870363e7b42f3a1ba45beaff90c7d54
Patchouli <self@patchouli.sh>
2024-11-08 13:18:45 -0500

    Refactor guard clauses and add inline documentation

--- source/Firecrawl.Extensions.DependencyInjection/FirecrawlServiceOptions.cs
+++ source/Firecrawl.Extensions.DependencyInjection/FirecrawlServiceOptions.cs
@@ -1,20 +1,25 @@
 ﻿using System;
 
+using Microsoft;
+using Microsoft.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace Firecrawl
 {
 	/// <summary>
-	/// 
+	/// Represents the options when registering an instance of the <see cref="FirecrawlService"/>
+	/// class with an <see cref="IServiceCollection"/>.
 	/// </summary>
 	public sealed class FirecrawlServiceOptions
 	{
 		/// <summary>
-		/// 
+		/// Gets or sets the base URL to use.
 		/// </summary>
 		public Uri BaseUrl { get; set; } =
 			FirecrawlService.DefaultBaseUrl;
 
 		/// <summary>
-		/// 
+		/// Gets or sets the API key to use.
 		/// </summary>
 		public string ApiKey { get; set; }
 	}
